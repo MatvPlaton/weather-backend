5d743007a83f9f83761d1df4586d97f2e534ddf8
UniLeonid <admin@smashup.ru>
2025-05-06 15:54:52 +0300

    Refactor create_app method

--- app.py
+++ app.py
@@ -9,82 +9,118 @@ from domain import ApiError, WeatherApi, WeatherState, WeatherRepository, \
     UserRepository, UserLoginRepository, User
 
 
-# flake8: noqa: C901
-def create_app(weather_api: WeatherApi, weather_repository: WeatherRepository,
-               user_repository: UserRepository,
-               user_login_repository: UserLoginRepository,
-               telegram_service_authorization_token: str):
+# Responses
+
+class WeatherResponse(BaseModel):
+    success: bool
+    temperature: float
+    feels_like: float
+    pressure: int
+    humidity: int
+
+class WeatherEntity(BaseModel):
+    time: float
+    temperature: float
+    feels_like: float
+    pressure: int
+    humidity: int
+
+class HistoryResponse(BaseModel):
+    success: bool
+    history: List[WeatherEntity]
+
+class UserResponse(BaseModel):
+    success: bool
+    telegram_id: int
+
+class SuccessfulLoginResponse(BaseModel):
+    success: bool
+    callback_url: str
+
+class EmptyResponse(BaseModel):
+    success: bool
+
+class ErrorResponse(BaseModel):
+    success: bool
+    error: str
+
+
+forbidden_response = {
+    "model": ErrorResponse,
+    "description": "Forbidden"
+}
+not_found_response = {
+    "model": ErrorResponse,
+    "description": "Not Found"
+}
+error_response = {
+    "model": ErrorResponse,
+    "description": "Internal Server Error"
+}
+
+
+def create_app(
+    weather_api: WeatherApi,
+    weather_repository: WeatherRepository,
+    user_repository: UserRepository,
+    user_login_repository: UserLoginRepository,
+    telegram_service_authorization_token: str
+):
     app = FastAPI()
-
-    class WeatherResponse(BaseModel):
-        success: bool
-        temperature: float
-        feels_like: float
-        pressure: int
-        humidity: int
-
-    class WeatherEntity(BaseModel):
-        time: float
-        temperature: float
-        feels_like: float
-        pressure: int
-        humidity: int
-
-    class HistoryResponse(BaseModel):
-        success: bool
-        history: List[WeatherEntity]
-
-    class UserResponse(BaseModel):
-        success: bool
-        telegram_id: int
-
-    class SuccessfulLoginResponse(BaseModel):
-        success: bool
-        callback_url: str
-
-    class EmptyResponse(BaseModel):
-        success: bool
-
-    class ErrorResponse(BaseModel):
-        success: bool
-        error: str
-
-    forbidden_response = {
-        "model": ErrorResponse,
-        "description": "Forbidden"
-    }
-    not_found_response = {
-        "model": ErrorResponse,
-        "description": "Not Found"
-    }
-    error_response = {
-        "model": ErrorResponse,
-        "description": "Internal Server Error"
-    }
-
-    def find_user(token) -> Union[User, JSONResponse]:
-        try:
-            user = user_repository.get_user(token)
-        except Exception as e:
-            return JSONResponse(
-                status_code=500,
-                content={
-                    "success": False,
-                    "error": f"Bad response from UserRepository: {e}"
-                }
-            )
-
-        if user is None:
-            return JSONResponse(
-                status_code=403,
-                content={
-                    "success": False,
-                    "error": "Bad token"
-                }
-            )
 
     # Weather
 
+    add_get_weather_route(app, weather_api, user_repository)
+    add_get_weather_history_route(app, weather_repository, user_repository)
+
+    # User
+
+    add_user_login_route(app, user_login_repository)
+    add_user_successful_login_route(
+        app,
+        telegram_service_authorization_token,
+        user_login_repository,
+        user_repository
+    )
+    add_user_telegram_id_route(
+        app,
+        user_repository
+    )
+
+    return app
+
+
+# Utils
+
+def find_user(user_repository, token) -> Union[User, JSONResponse]:
+    try:
+        user = user_repository.get_user(token)
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "success": False,
+                "error": f"Bad response from UserRepository: {e}"
+            }
+        )
+
+    if user is None:
+        return JSONResponse(
+            status_code=403,
+            content={
+                "success": False,
+                "error": "Bad token"
+            }
+        )
+
+# Routes
+# - Weather
+
+def add_get_weather_route(
+    app: FastAPI,
+    weather_api: WeatherApi,
+    user_repository: UserRepository
+):
     @app.get(
         "/weather",
         response_model=WeatherResponse,
@@ -95,7 +131,7 @@ def create_app(weather_api: WeatherApi, weather_repository: WeatherRepository,
     )
     def get_weather(city: str, user_token: str) -> \
             Union[WeatherResponse, ErrorResponse]:
-        user = find_user(user_token)
+        user = find_user(user_repository, user_token)
         if isinstance(user, JSONResponse):
             return user
 
@@ -116,6 +152,12 @@ def create_app(weather_api: WeatherApi, weather_repository: WeatherRepository,
                 status_code=500, content={"success": False, "error": msg}
             )
 
+
+def add_get_weather_history_route(
+    app: FastAPI,
+    weather_repository: WeatherRepository,
+    user_repository: UserRepository
+):
     @app.get(
         "/weather/history",
         response_model=HistoryResponse,
@@ -125,7 +167,7 @@ def create_app(weather_api: WeatherApi, weather_repository: WeatherRepository,
     )
     def get_weather_history(city: str | None, limit: int, user_token: str) -> \
             Union[HistoryResponse, ErrorResponse]:
-        user = find_user(user_token)
+        user = find_user(user_repository, user_token)
         if isinstance(user, JSONResponse):
             return user
 
@@ -158,8 +200,12 @@ def create_app(weather_api: WeatherApi, weather_repository: WeatherRepository,
                 }
             )
 
-    # User
+# - User
 
+def add_user_login_route(
+    app: FastAPI,
+    user_login_repository: UserLoginRepository
+):
     @app.post(
         "/user/login",
         response_model=EmptyResponse,
@@ -182,6 +228,12 @@ def create_app(weather_api: WeatherApi, weather_repository: WeatherRepository,
                 }
             )
 
+def add_user_successful_login_route(
+    app: FastAPI,
+    telegram_service_authorization_token: str,
+    user_login_repository: UserLoginRepository,
+    user_repository: UserRepository
+):
     @app.post(
         "/user/successful_login",
         response_model=SuccessfulLoginResponse,
@@ -241,6 +293,12 @@ def create_app(weather_api: WeatherApi, weather_repository: WeatherRepository,
         }
 
         return response
+
+
+def add_user_telegram_id_route(
+    app: FastAPI,
+    user_repository: UserRepository
+):
 
     @app.get(
         "/user/telegram_id",
